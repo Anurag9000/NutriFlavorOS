7f086e9d8f90570a84ed7b9d36e9a522b35b17af
Anurag9000 <anuragbasistha@gmail.com>
2026-08-07 09:28:33 +0530

    test: bound async UI convergence waits

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,4 +1,12 @@
 import "@testing-library/jest-dom";
+import { cleanup, configure } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+configure({ asyncUtilTimeout: 10_000 });
+
+afterEach(() => {
+  cleanup();
+});
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
